217a642db8d9c9ef6e05c62f6285415a17da2a2c
Noam Ben-Yechiel <nbenyechiel@twistbioscience.com>
2018-10-17 18:23:28 +0300

    Add sf oauth info to log

--- salesforce/__init__.py
+++ salesforce/__init__.py
@@ -15,7 +15,7 @@ import warnings
 
 import django
 
-__version__ = "0.7.5"
+__version__ = "0.7.6"
 
 DJANGO_18_PLUS = True  # unused by us now - backward compatibility
 DJANGO_19_PLUS = django.VERSION[:2] >= (1, 9)
