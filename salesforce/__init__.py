76be4df17abe4741bd12bfb91be9a8c074b98c7b
Noam Ben-Yechiel <nbenyechiel@twistbioscience.com>
2018-10-17 15:56:02 +0300

    Update version for caching purposes

--- salesforce/__init__.py
+++ salesforce/__init__.py
@@ -15,7 +15,7 @@ import warnings
 
 import django
 
-__version__ = "0.7.4"
+__version__ = "0.7.5"
 
 DJANGO_18_PLUS = True  # unused by us now - backward compatibility
 DJANGO_19_PLUS = django.VERSION[:2] >= (1, 9)
